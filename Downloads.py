e6f3f1c8a369d161ea3e0a8cddaac1617378fea7
nikolabelynski <67170221+nikolabelynski@users.noreply.github.com>
2020-06-25 22:09:07 +0000

    Update Downloads.py

--- Downloads.py
+++ Downloads.py
@@ -4,10 +4,11 @@ import os
 '''
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install sublime-text
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install sublime-text
 sudo apt-get install spotify-client
 '''
 
